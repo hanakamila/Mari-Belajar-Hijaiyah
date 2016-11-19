f05ccc498a5393def559799dec0acddd1a067294
tomama <nabilatamimi09@gmail.com>
2016-11-19 08:03:00 +0700

    Tampilan awal aplikasi Marbel Hijaiyah

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl208172635/marbelhijaiyah/About.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.project.xirpl208172635.marbelhijaiyah;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class About extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl208172635/marbelhijaiyah/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl208172635/marbelhijaiyah/MainActivity.java
@@ -1,13 +1,50 @@
 package id.sch.smktelkom_mlg.project.xirpl208172635.marbelhijaiyah;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    ImageButton pindah;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        final MediaPlayer SuaraButton = MediaPlayer.create(this, R.raw.button);
+        pindah = (ImageButton) findViewById(R.id.belajar);
+        pindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SuaraButton.start();
+                Intent IntenPindah = new Intent(MainActivity.this, Belajar.class);
+                startActivity(IntenPindah);
+            }
+        });
+
+        pindah = (ImageButton) findViewById(R.id.kuis);
+        pindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SuaraButton.start();
+                Intent IntenPindah = new Intent(MainActivity.this, Kuis.class);
+                startActivity(IntenPindah);
+            }
+        });
+
+        pindah = (ImageButton) findViewById(R.id.about);
+        pindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SuaraButton.start();
+                Intent IntenPindah = new Intent(MainActivity.this, About.class);
+                startActivity(IntenPindah);
+            }
+        });
     }
 }
